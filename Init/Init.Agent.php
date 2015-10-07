6314ca4e7cd813e1692dcc99af88b5daecf068d1
MarcilioPL <marcilio.p.l.15@gmail.com>
2015-10-06 21:45:01 -0300

    Update
    Fix

--- Init/Init.Agent.php
+++ Init/Init.Agent.php
@@ -8,8 +8,12 @@
 	define ( 'USER_AGENT_DOULCI',			'doulCiTeam/v 2.0.1 OMEGA' );
 	define ( 'USER_AGENT_INUVEN',			'iNuvenTeam/v1.0' );
 	define ( 'USER_AGENT_CAPTIVE',			'CaptiveNetworkSupport-277.10.5 wispr' );
-	
-	define ( 'USER_AGENT_ACTIVATION',		'iTunes/11.1.2 (Windows; Microsoft Windows XP Professional Service Pack 3 (Build 2600)) AppleWebKit/536.30.1' );
+
+
+	//
+	//
+#	define ( 'USER_AGENT_ACTIVATION',		'iTunes/11.1.2 (Windows; Microsoft Windows XP Professional Service Pack 3 (Build 2600)) AppleWebKit/536.30.1' );
+	define ( 'USER_AGENT_ACTIVATION',		' iTunes/12.0.1 (Windows; Microsoft Windows 7 x64 Business Edition Service Pack 1 (Build 7601)) AppleWebKit/7600.1017.0.24' );
 	define ( 'USER_AGENT_ACTIVATION_MAC',	'iTunes/11.2.1 (Macintosh; OS X 10.9.2) AppleWebKit/537.75.14' );
 	define ( 'USER_AGENT_IOS_DEVICE',		'iOS Device Activator (MobileActivation-20 built on Nov  2 2011 at 20:18:00)' );
 	define ( 'USER_AGENT_CERTIFYME',		'iOS Device Activator (MobileActivation-20 built on Nov  2 2011 at 20:18:00) MobileActivationNSURLConnection (MobileActivation-20 built on Nov  2 2011 at 20:18:02)' );
